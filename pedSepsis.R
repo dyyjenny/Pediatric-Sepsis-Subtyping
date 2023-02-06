4083a5bfe6da65552b758fe105c152edbb8e43a5
ZhidiLuo <115842835+ZhidiLuo@users.noreply.github.com>
2023-02-06 14:13:25 -0600

    update 2.6.2023

--- pedSepsis.R
+++ pedSepsis.R
@@ -1,6 +1,8 @@
 library(readr)
 library(dplyr)
 library(zoo)
+library(ggplot2)
+library(reshape2)
 
 momentum = 4
 
@@ -11,6 +13,7 @@ vital_trajectory <- read_csv("C:/Users/zlr5321/Desktop/HSIPCourse/Quarter2/HSIP 
 allVitals = unique(vital_trajectory$event_name)
 vitalGrid = expand.grid(unique(vital_trajectory$EncID), 0:12)
 names(vitalGrid) = c("EncID","event_hour")
+vitalGrid = merge(vitalGrid, unique(vital_trajectory[,c("EncID","hospital_id","age_group","mods_day7","died_in_hosp","died_hour_round")]), by = "EncID")
 
 for (thisVital in allVitals)
 {
@@ -18,12 +21,7 @@ for (thisVital in allVitals)
   vital1 = vital_trajectory[vital_trajectory$event_name == thisVital,c("EncID","event_hour","died_hour_round","event_value")]
   vital1 = vital1[!is.na(vital1$event_value),]
   vital1$imputed = 0
-  # vitalImputeLeft = vital1[order(vital1$EncID, vital1$event_hour),]
-  # n = dim(vitalImputeLeft)[1]
-  # ind = c(1,1+which(vitalImputeLeft[1:(n-1),]$EncID != vitalImputeLeft[2:n,]$EncID))
-  # vitalImputeLeft = vitalImputeLeft[ind,]
-  # vitalImputeLeft$imputed = 1
-  
+
   vitalImputeRight = vital1[order(vital1$EncID, -vital1$event_hour),]
   n = dim(vitalImputeRight)[1]
   ind = c(1,1+which(vitalImputeRight[1:(n-1),]$EncID != vitalImputeRight[2:n,]$EncID))
@@ -32,18 +30,11 @@ for (thisVital in allVitals)
   
   for (i in (1:momentum))
   {
-    # vitalImputeLefti = vitalImputeLeft
-    # vitalImputeLefti$event_hour = vitalImputeLefti$event_hour - i
-    # vitalImputeLefti = vitalImputeLefti[vitalImputeLefti$event_hour >= 0,]
-    # vital1 = rbind(vital1, vitalImputeLefti)
-    
     vitalImputeRighti = vitalImputeRight
     vitalImputeRighti$event_hour = vitalImputeRighti$event_hour + i
     vitalImputeRighti = vitalImputeRighti[vitalImputeRighti$event_hour <= 12,]
     vital1 = rbind(vital1, vitalImputeRighti)
   }
-  
-  
   
   vitalExpanded = expand.grid(unique(vital1$EncID), 0:12)
   names(vitalExpanded) = c("EncID","event_hour")
@@ -51,35 +42,82 @@ for (thisVital in allVitals)
   vitalExpanded = vitalExpanded[order(vitalExpanded$EncID, vitalExpanded$event_hour),]
   
   ind = which(!is.na(vitalExpanded$imputed) & vitalExpanded$imputed == 0)
-  vitalExpanded$imputeEx = 0
+  vitalExpanded$imputeEx = NA
   
   for (i in 1:momentum)
-  {
-    # vitalExpanded[ind[floor((ind-1-i)/13) == floor((ind-1)/13)]-i,]$imputeEx = 1
     vitalExpanded[ind[floor((ind-1+i)/13) == floor((ind-1)/13)]+i,]$imputeEx = 1
-  }
+
   vitalExpanded[is.na(vitalExpanded$imputed),]$imputed = vitalExpanded[is.na(vitalExpanded$imputed),]$imputeEx
   vitalExpanded = vitalExpanded %>% mutate(event_value_Interp = na.approx(event_value, na.rm = FALSE))
-  
-  if (thisVital == "FIO2")
-    vitalExpanded[is.na(vitalExpanded$imputed) | vitalExpanded$imputed == 1, ]$event_value = 0.21
   
   vitalExpanded$died_hour_round = ave(vitalExpanded$died_hour_round, vitalExpanded$EncID, FUN = function(x) mean(x, na.rm = TRUE))
   treatasNA = is.na(vitalExpanded$imputed) | (!is.na(vitalExpanded$died_hour_round) & vitalExpanded$event_hour > vitalExpanded$died_hour_round)
   if (any(treatasNA))
     vitalExpanded[treatasNA,]$event_value_Interp = NA
   
+  if (thisVital == "SF_RATIO")
+  {
+    vitalGridR = vitalGrid
+    vitalGridR$SF_R = vitalGridR$PULSE_OXImputed/vitalGridR$FIO2Imputed
+    vitalExpanded = merge(vitalExpanded, vitalGridR[!is.na(vitalGridR$SF_R),c("EncID","event_hour","SF_R")], by = c("EncID","event_hour"), all.x = T, all.y = T)
+    vitalExpanded[is.na(vitalExpanded$imputed) | vitalExpanded$imputed != 0,]$event_value_Interp = vitalExpanded[is.na(vitalExpanded$imputed) | vitalExpanded$imputed != 0,]$SF_R
+  }
   names(vitalExpanded)[names(vitalExpanded) == "event_value_Interp"] = paste0(thisVital, "Imputed")
   
   vitalGrid = merge(vitalGrid, vitalExpanded[,c("EncID","event_hour", paste0(thisVital, "Imputed"))], by = c("EncID","event_hour"), all.x = T)
+  
+  if (thisVital == "FIO2")  
+    vitalGrid[is.na(vitalGrid$FIO2Imputed) & (is.na(vitalGrid$died_hour_round) | vitalGrid$event_hour <= vitalGrid$died_hour_round), ]$FIO2Imputed = 0.21
+  
 }
 
-vitalGrid = merge(vitalGrid, unique(vital_trajectory[,c("EncID","hospital_id","age_group","mods_day7","died_in_hosp","died_hour_round")]), by = "EncID")
+for (thisVital in allVitals)
+  vitalGrid[,paste0(thisVital, "Imputed")] = (vitalGrid[,paste0(thisVital, "Imputed")] - mean(vitalGrid[,paste0(thisVital, "Imputed")], na.rm = T))/sd(vitalGrid[,paste0(thisVital, "Imputed")],na.rm = T)
+
 
 write.csv(vitalGrid, file = "C:/Users/zlr5321/Desktop/HSIPCourse/Quarter2/HSIP 442 Informatics II/vitals_trajectory_imputed.csv")
 
 
+# Graphs and stats
+ 
+vitalGrid = read_csv("C:/Users/zlr5321/Desktop/HSIPCourse/Quarter2/HSIP 442 Informatics II/vitals_trajectory_imputed.csv")
+
+vitalGrid$predead = 1
+vitalGrid[!is.na(vitalGrid$died_hour_round) & vitalGrid$died_hour_round < vitalGrid$event_hour,]$predead = 0
+vitalGridNM = vitalGrid[vitalGrid$predead == 1,]
+
+allVitals = c("FIO2Imputed","MAPImputed","PULSEImputed","PULSE_OXImputed","RESP_RATEImputed","SBPImputed","SF_RATIOImputed","TEMPImputed")
+dataStats = data.frame(num.Complete = rep(NA, 7), num.missing = rep(NA, 7), miss.percent.among.missing = rep(NA, 7))
+plotData = data.frame(matrix(nrow = 12,ncol = 8))
+names(plotData) = allVitals
+
+k = 1
+for (thisVital in allVitals)
+{
+  thisVitalPercent = aggregate(vitalGridNM[,thisVital], list(vitalGridNM$EncID), FUN = function(x) sum(is.na(x))/length(x))
+  thisVitalPercent = thisVitalPercent[,2]
+  dataStats[k,]$num.Complete = sum(thisVitalPercent == 0)
+  dataStats[k,]$num.missing = sum(thisVitalPercent > 0)
+  dataStats[k,]$miss.percent.among.missing = mean(thisVitalPercent[thisVitalPercent > 0])
+  for (i in 1:12)
+    plotData[i,thisVital] = mean(unlist(vitalGridNM[vitalGridNM$event_hour == i,thisVital]), na.rm = T)
+  k = k + 1
+}
+
+thisVitalPercent = aggregate(rowSums(vitalGridNM[,allVitals[c(2,3,5:8)]]), list(vitalGridNM$EncID), FUN = function(x) sum(is.na(x))/length(x))
+thisVitalPercent = thisVitalPercent[,2]
+dataStats[k,]$num.Complete = sum(thisVitalPercent == 0)
+dataStats[k,]$num.missing = sum(thisVitalPercent > 0)
+dataStats[k,]$miss.percent.among.missing = mean(thisVitalPercent[thisVitalPercent > 0])
+
+plotData$time = 1:12
+plotData = melt(plotData, id.vars = "time", variable.name = "series")
+ggplot(plotData, aes(time, value)) + geom_line(aes(colour = series))
+
+write.csv(dataStats, file = "C:/Users/zlr5321/Desktop/HSIPCourse/Quarter2/HSIP 442 Informatics II/data.csv")
 
 
-
+vitalGridNM$na.percent = ave(rowSums(vitalGridNM[,allVitals[c(2,3,5:8)]]), vitalGridNM$EncID, FUN = function(x) sum(is.na(x))/length(x))
+vitalGridnoNA = vitalGrid[is.element(vitalGrid$EncID, vitalGridNM[vitalGridNM$na.percent == 0,]$EncID),]
+write.csv(vitalGridnoNA, file = "C:/Users/zlr5321/Desktop/HSIPCourse/Quarter2/HSIP 442 Informatics II/vitals_trajectory_imputed_noNA.csv")
 
