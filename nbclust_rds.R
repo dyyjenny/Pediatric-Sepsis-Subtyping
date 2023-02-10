263775c96a8885f0757c4ad3303cf20ec6acadf9
dyyjenny <53281037+dyyjenny@users.noreply.github.com>
2023-02-10 14:03:46 -0600

    added hac results on dtw matrix

--- nbclust_rds.R
+++ nbclust_rds.R
@@ -1,9 +1,21 @@
 library(NbClust)
 
-df = readRDS('/home/ydn4687/sepsis/dtw_out.Rds')
+df = read.csv('/home/ydn4687/sepsis/dtw_out.csv')
 
-res<-NbClust(df, distance = "euclidean", min.nc=2, max.nc=4, 
-            method = "ward.D", index = "all")
+#res<-NbClust(df, distance = "euclidean", min.nc=2, max.nc=4, 
+            #method = "ward.D", index = "all")
 
-print(res)
+list.methods = c( "kl", "ch", "hartigan", "ccc","scott", "marriot", "trcovw", "tracew", "friedman", "rubin", "cindex", "db", "silhouette", "duda", "pseudot2", "beale", "ratkowsky", "ball", "ptbiserial", "gap", "frey", "mcclain", "gamma", "gplus", "tau", "dunn", "hubert", "sdindex", "dindex", "sdbw")
+
+#tabla = as.data.frame(matrix(ncol = 2, nrow = length(lista.methods)))
+#names(tabla) = "euclidean"
+
+for(i in 1:length(list.methods)){
+    nb = NbClust(df, distance = "euclidean",
+                 min.nc = 2, max.nc = 6, 
+                 method = "ward.D", index = list.methods[i])
+    write.csv(nb$Best.nc[1], paste('/home/ydn4687/sepsis/nbclust',list.methods[i], 'csv', sep = '.'))
+}
+
+
 
